Fix noise scaling bug

neurogrid/decoders.py
@@ -5,7 +5,7 @@ def classic(activity, targets, rng, noise=0.1):
     A = activity
     X = targets
     
-    A += rng.randn(*A.shape)*noise
+    A += rng.randn(*A.shape)*(noise*np.max(A))
     
     G = np.dot(A, A.T)
     U = np.dot(A, X)
@@ -19,7 +19,7 @@ def lstsq(activity, targets, rng, noise=0.1):
     A = activity
     X = targets
     
-    A += rng.randn(*A.shape)*noise
+    A += rng.randn(*A.shape)*(noise*np.max(A))
     
     return np.linalg.lstsq(A.T, X)[0]
     
@@ -27,7 +27,7 @@ def nonnegative(activity, targets, rng, noise=0.1):
     A = activity
     X = targets
     
-    A += rng.randn(*A.shape)*noise
+    A += rng.randn(*A.shape)*(noise*np.max(A))
     
     d = []
     for i in range(X.shape[1]):