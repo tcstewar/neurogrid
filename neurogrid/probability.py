b287eb7bdd70c3032e3b56808abb472308d2d9b8
Terry Stewart <tcstewar@uwaterloo.ca>
2013-06-15 06:42:36 -0400

    Fix calc max weight

--- neurogrid/probability.py
+++ neurogrid/probability.py
@@ -2,8 +2,7 @@ import numpy as np
 
 def convert_weights(w, max_weight=None, bits=None):
     if max_weight is None: 
-        max_weight = np.max(w)
-        
+        max_weight = np.max(np.abs(w))
     p = np.abs(w)/max_weight
     w = np.where(w>=0, max_weight, -max_weight)
     
