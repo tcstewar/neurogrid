9fdb1e82ed899379f2ad3e330751297cfe33a097
Terry Stewart <tcstewar@uwaterloo.ca>
2013-06-15 04:14:45 -0400

    Add dendrites, avg rate measurement to ensembles

--- neurogrid/ensemble.py
+++ neurogrid/ensemble.py
@@ -4,23 +4,16 @@ from . import cache
 from . import samples
 from . import activity
 from . import decoders
-
+from . import dendrites
 
 import numpy as np
 
 class Ensemble:
-    def __init__(self, rows, cols, dimensions, seed, encoder_type='random', bias=500, balanced=True, nonlinear=0):
+    def __init__(self, rows, cols, dimensions, seed, encoder_type='random', bias=500, balanced=True, nonlinear=0, dendrite_width=None, input_scale=0.005):
         self.seed = seed
         rng = np.random.RandomState(seed)        
         self.rngs = [np.random.RandomState(rng.randint(0,0x7fffffff)) for i in range(10)]
-        
-        self.cache_neurons = cache.Item(name='neurons', N=rows*cols, seed=seed, bias=bias, balanced=balanced, nonlinear=nonlinear)
-        self.neurons = self.cache_neurons.get()
-        if self.neurons is None:
-            self.neurons = neurons.SpikeNeuron(rows*cols, self.rngs[0], bias=bias, balanced=balanced, nonlinear=nonlinear)
-            self.cache_neurons.set(self.neurons)
 
-        
         self.cache_encoders = cache.Item(name='encoders', encoder_type=encoder_type, 
                         rows=rows, cols=cols, dimensions=dimensions, 
                         seed=seed)
@@ -35,12 +28,36 @@ class Ensemble:
             elif encoder_type=='kohonen':
                 self.encoders = encoders.kohonen(rows*cols, dimensions, self.rngs[1], rows, cols, iterations=200)
             self.cache_encoders.set(self.encoders)    
+
+        
+        self.cache_neurons = cache.Item(name='neurons', N=rows*cols, seed=seed, bias=bias, balanced=balanced, nonlinear=nonlinear, dendrite_width=dendrite_width, input_scale=input_scale)
+        self.neurons = self.cache_neurons.get()
+        if self.neurons is None:
+            dendrite = None
+            if dendrite_width is not None:
+                dendrite = dendrites.blur_kernel(rows, cols, dendrite_width)
+        
+            self.neurons = neurons.SpikeNeuron(rows*cols, self.rngs[0], bias=bias, balanced=balanced, nonlinear=nonlinear, dendrite=dendrite, input_scale=input_scale)
             
+            self.cache_neurons.set(self.neurons)
+
+    def get_average_rate(self, fc=500, fr=500):
+        X, A = activity.classic(self.neurons, self.encoders, self.rngs[6], fc=fc, fr=fr) 
+        return np.sum(A)/(A.shape[0]*A.shape[1])
+                
     def get_decoder(self, name='X', func=None, mode='classic', fc=500, fr=500):
         item = cache.Item(name='decoder', decoder_name=name, seed=self.seed, neurons=self.cache_neurons, encoders=self.cache_encoders, mode=mode, fc=fc, fr=fr)
         d = item.get()
         if d is None:
-            X, A = activity.classic(self.neurons, self.encoders, self.rngs[2], fc=fc, fr=fr)    
+            X, A = activity.classic(self.neurons, self.encoders, self.rngs[2], fc=fc, fr=fr) 
+
+            #import pylab
+            #pylab.plot(X, A.T)
+            #coverage = np.sum(np.where(A.T>0, 1.0, 0.0))/(A.shape[0]*A.shape[1])
+            #max_rate = np.max(A)
+            #avg_rate = np.sum(A)/(A.shape[0]*A.shape[1])
+            #pylab.title('Coverage: %1.3f  Max:%1.3f  Avg:%1.3f'%(coverage, max_rate, avg_rate))
+            #pylab.show()
             
             if func is not None:
                 X = func(X)
