1ef81b6fa85b061574b1c7cb170249959868ad06
Terry Stewart <tcstewar@uwaterloo.ca>
2013-06-15 04:13:24 -0400

    Make dendrites more like neurogrid

--- neurogrid/dendrites.py
+++ neurogrid/dendrites.py
@@ -1,8 +1,8 @@
 import numpy as np
 import math
 
-def blur_kernel(rows, cols, width):
-    kernel = np.zeros((rows, cols))
+def blur_kernel(rows, cols, width, surround=True):
+    kernel = np.zeros((rows, cols), dtype='f')
     if width == 0:
         kernel[0, 0] = 1
     else:
@@ -13,15 +13,39 @@ def blur_kernel(rows, cols, width):
                 dy = j
                 if dy>cols/2: dy-=cols
             
-                distance = math.sqrt(dx*dx + dy*dy)
+                distance = float(abs(dx)+abs(dy))#math.sqrt(dx*dx + dy*dy)
                 kernel[i, j] = math.exp(-distance/width)
 
         kernel /= np.sum(kernel)
+    kernel = np.roll(kernel, (rows+1)/2-1, axis=0)
+    kernel = np.roll(kernel, (cols+1)/2-1, axis=1)
+    
+    if surround:
+        kernel = scipy.signal.convolve2d(kernel, surround_kernel(rows, cols), mode='same')
+    
     return kernel    
+    
+def surround_kernel(rows, cols):
+    kernel = np.zeros((rows, cols), dtype='f')
+    kernel[1, -1] = 1
+    kernel[1, 0] = 1
+    kernel[1, 1] = 1
+    
+    kernel[-1, -1] = 1
+    kernel[-1, 0] = 1
+    kernel[-1, 1] = 1
+    
+    kernel[0, -1] = 1
+    kernel[0, 1] = 1
+    kernel /= np.sum(kernel)
+    kernel = np.roll(kernel, (rows+1)/2-1, axis=0)
+    kernel = np.roll(kernel, (cols+1)/2-1, axis=1)
+    return kernel    
+        
     
     
 import scipy.signal    
-def apply(matrix, kernel):
+def apply_matrix(matrix, kernel):
     N, D = matrix.shape
     assert N == kernel.shape[0] * kernel.shape[1]
     
@@ -33,3 +57,14 @@ def apply(matrix, kernel):
         row.shape = (N,)
         r.append(row)
     return np.array(r).T    
+
+def apply_vector(vector, kernel):
+    N, = vector.shape
+    assert N == kernel.shape[0] * kernel.shape[1]
+    v = vector[:]
+    v.shape = kernel.shape
+    row = scipy.signal.convolve2d(v, kernel, mode='same')
+    row.shape = (N,)
+    return row    
+    
+    
