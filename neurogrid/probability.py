35f5ce656de05148ecd4c03567507221ab058d6e
Terry Stewart <tcstewar@uwaterloo.ca>
2013-06-15 04:15:42 -0400

    Add bit limit to probabilistic weights

--- neurogrid/probability.py
+++ neurogrid/probability.py
@@ -1,10 +1,13 @@
 import numpy as np
 
-def convert_weights(w, max_weight=None):
+def convert_weights(w, max_weight=None, bits=None):
     if max_weight is None: 
         max_weight = np.max(w)
         
     p = np.abs(w)/max_weight
     w = np.where(w>=0, max_weight, -max_weight)
     
+    if bits is not None:
+        p = np.round(p*(2**bits))/(2**bits)
+        
     return p, w
